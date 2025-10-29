d47c8107fd9098d5d94f69d912284a3555ec1a13
mtoliveshotel <mtolives@gmail.com>
2025-10-29 15:52:54 +0200

    Delete ascension-astro directory

--- ascension-astro/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-  site: 'https://preview.ascension.tours',
-  integrations: [sitemap()],
-});
