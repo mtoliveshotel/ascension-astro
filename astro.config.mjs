b696184a30ef5313b27551a98f0bc4a4b75bdf88
mtoliveshotel <mtolives@gmail.com>
2025-10-29 17:20:59 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://preview.ascension.tours',
+  site: 'https://mtoliveshotel.github.io/ascension-astro',
+  base: '/ascension-astro/',           // ← IMPORTANT for GitHub Pages
   integrations: [sitemap()],
 });
